35675f6d3f39a799a6dc8b1fcbf575161bb1af92
Ignacio Damián González <igonzalez@startuniversity.net>
2023-04-09 08:40:27 -0300

    pages

---
+++ app/about/page.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
+        consequatur qui laboriosam. Tempora vero quidem odit at recusandae
+        incidunt dolorum quod ex voluptatum voluptatem magni eveniet commodi
+        unde, libero quibusdam itaque sequi, hic animi! Debitis ipsum quas vel,
+        dolorum accusantium quidem laudantium quisquam sint omnis nihil eligendi
+        sunt nisi, perferendis adipisci reprehenderit magnam dignissimos unde.
+        Architecto, saepe neque. At ex libero sed earum saepe impedit, a laborum
+        dignissimos corporis eaque, aliquam blanditiis culpa, dolores ratione
+        repudiandae nesciunt cum cumque esse. Ad possimus libero quia, vel saepe
+        suscipit vitae illum culpa fugit in ab asperiores perferendis tempora
+        corporis quaerat eum. Excepturi?
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ app/services/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function ServicePage() {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nemo dolor
+        atque quod, laudantium odio commodi recusandae. Quas quasi facilis quia
+        rerum odit deserunt quam eaque aliquam, neque perspiciatis debitis
+        delectus aut libero doloribus tempora velit ullam rem quibusdam possimus
+        impedit alias nulla id? Quo voluptatibus obcaecati dolorum quaerat vero
+        aut deserunt sint quae rem, nostrum laboriosam debitis aliquam et?
+      </p>
+    </div>
+  );
+}
+
+export default ServicePage;
