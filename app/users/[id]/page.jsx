e0168481e80bbeff91aefefd5d384f451dd9d742
Ignacio Damián González <igonzalez@startuniversity.net>
2023-04-09 09:50:41 -0300

    user details

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,7 +1,23 @@
-function UsersPage() {
+async function getUser(id) {
+  const res = await fetch(`https://reqres.in/api/users/${id}`);
+  const data = await res.json();
+  return data.data;
+}
+
+async function UsersPage({ params }) {
+  const user = await getUser(params.id);
   return (
     <div>
       <h1>User details</h1>
+      <div>
+        <img src={user.avatar} alt={user.first_name} />
+        <div>
+          <h3>
+            {user.id} {user.first_name} {user.last_name}
+          </h3>
+          <p>{user.email}</p>
+        </div>
+      </div>
     </div>
   );
 }
